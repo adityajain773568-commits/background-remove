d27fc5926164fdaa772ba077bbe3a8909e2286e1
aditya <adityajain773568@gmail.com>
2026-04-26 13:13:21 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Menubar from "./components/Menubar.jsx";
+function App() {
+    return (
+        <>
+            <div>
+                <Menubar/>
+            </div>
+        </>
+    )
+}
+export default App
